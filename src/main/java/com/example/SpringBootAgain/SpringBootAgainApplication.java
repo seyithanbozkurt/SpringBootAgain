d9006a11b5f1656633908e3f707c41e0815e8e91
seyithan3457 <seyithan3457@gmail.com>
2026-02-10 11:39:47 +0300

    Initial commit

---
+++ src/main/java/com/example/SpringBootAgain/SpringBootAgainApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootAgain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAgainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootAgain/SpringBootAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootAgain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAgainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
