45bc4041f26a7e5cc3707232841b550a02a63f20
seyithan3457 <seyithan3457@gmail.com>
2026-02-11 11:51:23 +0300

    Loombok eklendi

---
+++ src/main/java/com/example/SpringBootAgain/model/User.java
@@ -0,0 +1,15 @@
+package com.example.SpringBootAgain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String name;
+    //sdfasfda
+}
