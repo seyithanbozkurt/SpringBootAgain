4076bf435a3ea0bd3750608df01711e97781409c
seyithan3457 <seyithan3457@gmail.com>
2026-02-22 02:04:52 +0300

    Spring Data Jpa ile Get isteği atılarak tüm studentleri listeleyen method yazıldı.

--- spring-data-jpa/src/main/java/com/example/spring_data_jpa/service/impl/StudentServiceimpl.java
+++ spring-data-jpa/src/main/java/com/example/spring_data_jpa/service/impl/StudentServiceimpl.java
@@ -5,6 +5,9 @@ import com.example.spring_data_jpa.repository.StudentRepository;
 import com.example.spring_data_jpa.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Service
 public class StudentServiceimpl implements IStudentService {
@@ -15,5 +18,12 @@ public class StudentServiceimpl implements IStudentService {
     @Override
     public Student saveStudent(Student student) {
         return studentRepository.save(student);
+    }
+
+
+    @Override
+    public List<Student> GetAllStudents() {
+        List<Student> students = studentRepository.findAll();
+        return students;
     }
 }
