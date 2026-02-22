174dff9ae45b9c07b53b1511fc57e7a788dfd09d
seyithan3457 <seyithan3457@gmail.com>
2026-02-23 01:45:28 +0300

    DTO katmanı oluşturdu. servisler dtolara göre düzenlendi.

--- spring-data-jpa/src/main/java/com/example/spring_data_jpa/service/IStudentService.java
+++ spring-data-jpa/src/main/java/com/example/spring_data_jpa/service/IStudentService.java
@@ -1,13 +1,15 @@
 package com.example.spring_data_jpa.service;
 
+import com.example.spring_data_jpa.dto.DTOStudent;
+import com.example.spring_data_jpa.dto.DTOStudentIU;
 import com.example.spring_data_jpa.entity.Student;
 
 import java.util.List;
 
 public interface IStudentService {
-    public Student saveStudent(Student student);
-    public List<Student> GetAllStudents();
-    public Student getStudentByID(Integer id);
+    public DTOStudent saveStudent(DTOStudentIU dtoStudentIU);
+    public List<DTOStudent> GetAllStudents();
+    public DTOStudent getStudentByID(Integer id);
     public void deleteStudentById(Integer id);
-    public Student updateStudent(Integer id, Student updateStudent);
+    public DTOStudent updateStudent(Integer id, DTOStudentIU updateStudentIU);
 }
