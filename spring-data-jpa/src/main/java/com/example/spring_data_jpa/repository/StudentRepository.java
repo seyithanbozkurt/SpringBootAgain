6237f9d715765e23239dcb4c04decc574a693b98
seyithan3457 <seyithan3457@gmail.com>
2026-02-23 15:44:44 +0300

    @Query anatasyonu kullanımı gerçekleşitirildi.

--- spring-data-jpa/src/main/java/com/example/spring_data_jpa/repository/StudentRepository.java
+++ spring-data-jpa/src/main/java/com/example/spring_data_jpa/repository/StudentRepository.java
@@ -2,8 +2,20 @@ package com.example.spring_data_jpa.repository;
 
 import com.example.spring_data_jpa.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Integer> {
+    //HQL : SINIFIN İSMİ VE DEĞİŞKENLERİN ADLARI İLE SORGULAR YAZILIR.
+    //SQL : DB DE Kİ TABLO İSİMLERİ VE TABLO İÇİNDEKİ KOLANLAR KULLANILARAK YAZILIR.
+
+    @Query(value = "from Student", nativeQuery = false)
+    List<Student> findAllStudents();
+
+    @Query(value = "from Student s Where  s.id= :studentid")
+    Optional<Student> findStudentById(Integer studentid);
 }
